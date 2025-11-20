647fcb447a43119cd02261fb09fe92d93f9aa37f
AzuraTen4 <azurablade11@gmail.com>
2025-11-20 16:10:01 +0700

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Made with Love :heart:',
+    text: 'Made with Love',
   },
 }
 
