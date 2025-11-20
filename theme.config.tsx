c18b507b70ea9820d494c306488e91b07c5edd0c
AzuraTen4 <azurablade11@gmail.com>
2025-11-20 15:48:41 +0700

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,10 +4,10 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>For The Kingdom Guide</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/AzuraTen4/documentation-starter-kit',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/QB9xDPKXwT',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
