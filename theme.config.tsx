b6f0bd3094eb38024f5949385a44abad2701a400
Azuraten4 <azurablade11@gmail.com>
2025-11-22 02:54:51 +0700

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,20 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  logo: (
+    <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+      {/* Logo dari public folder */}
+      <img 
+        src="/gambar/logo.png" 
+        alt="For The Kingdom Logo" 
+        width={32} 
+        height={32}
+      />
+      <span style={{ fontWeight: 'bold', fontSize: '18px' }}>
+        For The Kingdom Guide
+      </span>
+    </div>
+  ),
   logo: <span>For The Kingdom Guide</span>,
   project: {
     link: 'https://github.com/AzuraTen4',
