cf85a2e39adb330826eabbb6a0a7f709a6328750
Azuraten4 <azurablade11@gmail.com>
2025-11-28 19:41:30 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <SpeedInsights />
+      <Analytics />
     </>
   );
 }
