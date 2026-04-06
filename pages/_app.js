8b7ec471d337a6424ef2050081f59ac42ce842ac
Azuraten4 <azurablade11@gmail.com>
2026-04-06 20:20:36 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import 'react-medium-image-zoom/dist/styles.css'
 
 function MyApp({ Component, pageProps }) {
   return (
