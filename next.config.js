c9ad71e361445c784eefd0f6fb05033b800843be
Azuraten4 <azurablade11@gmail.com>
2026-04-06 16:09:57 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,5 +8,12 @@ const withNextra = nextra({
 export default withNextra({
   images: {
     unoptimized: true
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || []
+      config.externals.push('react-medium-image-zoom')
+    }
+    return config
   }
 })
