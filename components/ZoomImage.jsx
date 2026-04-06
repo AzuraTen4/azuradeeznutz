a89ba3ea754e392cd05b7850413b75657a6909fe
Azuraten4 <azurablade11@gmail.com>
2026-04-06 18:19:51 +0700

    Refactor ZoomImage component for cleaner code

--- components/ZoomImage.jsx
+++ components/ZoomImage.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
-import 'react-medium-image-zoom/dist/styles.css'
 
-// Mengimpor library zoom hanya di sisi klien (browser)
+// import zoom hanya di client
 const Zoom = dynamic(() => import('react-medium-image-zoom'), { ssr: false })
 
 const ZoomImage = ({ src, alt, ...props }) => {
   return (
     <Zoom>
-      {/* Gunakan tag img standar agar kompatibel dengan library zoom */}
-      <img 
-        src={src} 
-        alt={alt} 
-        style={{ width: '100%', height: 'auto', borderRadius: '8px' }} 
-        {...props} 
+      <img
+        src={src}
+        alt={alt}
+        style={{ width: '100%', height: 'auto', borderRadius: '8px' }}
+        {...props}
       />
     </Zoom>
   )
