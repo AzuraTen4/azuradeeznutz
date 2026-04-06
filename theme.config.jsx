1fa37e9c66d33b882ada3e6b506e62a3a52029ab
Azuraten4 <azurablade11@gmail.com>
2026-04-07 03:01:20 +0700

    Update theme.config.jsx

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-
 const config = {
-
   useNextSeoProps() {
     return {
       titleTemplate: '%s – For The Kingdom Guide'
     }
   },
-
   logo: (
     <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
       <img 
@@ -22,35 +19,31 @@ const config = {
       </span>
     </div>
   ),
-
   head: (
     <>
       <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </>
   ),
-
   project: {
     link: 'https://github.com/AzuraTen4',
   },
-
   chat: {
     link: 'https://discord.gg/QB9xDPKXwT',
   },
-
   footer: {
     text: 'Made With Love ❤️ © AzuraTen4',
   },
-
   feedback: {
     content: null,
   },
-
   editLink: {
     component: null,
   },
-
-  docsRepositoryBase: 'https://github.com/AzuraTen4/accumulation-starter-kit/blob/main'
+  docsRepositoryBase: 'https://github.com/AzuraTen4/accumulation-starter-kit/blob/main',
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true
+  }
 }
-
 export default config
