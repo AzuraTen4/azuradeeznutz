005543b7e0fed3fa2d7b597500993c437638ba6a
Azuraten4 <azurablade11@gmail.com>
2026-04-06 19:51:06 +0700

    Update mdx-components.js

--- mdx-components.js
+++ mdx-components.js
@@ -1,24 +1,11 @@
-import Image from 'next/image'
-
 export function useMDXComponents(components) {
   return {
-    img: ({ src, alt, ...props }) => {
-      // kalau GIF → pakai <img> biasa
-      if (src.endsWith('.gif')) {
-        return <img src={src} alt={alt || ''} style={{ width: '100%' }} />
-      }
-
-      // selain GIF → pakai next/image
-      return (
-        <Image
-          src={src}
-          alt={alt || ''}
-          width={800}
-          height={400}
-          style={{ width: '100%', height: 'auto' }}
-        />
-      )
-    },
+    img: (props) => (
+      <img
+        {...props}
+        style={{ width: '100%', borderRadius: '8px' }}
+      />
+    ),
     ...components,
   }
-        }
+      }
