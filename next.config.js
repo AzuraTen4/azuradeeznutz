d511013f45790561b30506c8f246033d9dcfd8a3
Azuraten4 <azurablade11@gmail.com>
2026-04-06 16:06:24 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 import nextra from 'nextra'
 
 const withNextra = nextra({
-  // Config baru untuk Nextra v4
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx'
-  // Hapus properti lama
 })
 
-export default withNextra()
+export default withNextra({
+  images: {
+    unoptimized: true
+  }
+})
