f76b7bcd8fd500c2573a84db87afb3fde28016b3
Azuraten4 <azurablade11@gmail.com>
2026-04-06 16:16:31 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import dynamic from 'next/dynamic';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,3 +13,7 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
+export const config = {
+  unstable_runtimeJS: true
+};
