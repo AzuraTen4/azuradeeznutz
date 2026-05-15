639bf896e985283c035a0ada98c35b22315b78e3
Azuraten4 <azurablade11@gmail.com>
2026-05-15 16:39:12 +0700

    Update theme.config.jsx

--- theme.config.jsx
+++ theme.config.jsx
@@ -57,7 +57,7 @@ banner: {
   key: 'ftk-banner',
   content: (
     <span style={{ background: '#5865F2', padding: '4px 12px', borderRadius: '4px' }}>
-      🎮 For The Kingdom is now in Maintenance Road to Mainnet! Join → {' '}
+      🎮 For The Kingdom is now LIVE on Mainnet! Join for more info → {' '}
       <a href="https://discord.gg/QB9xDPKXwT" target="_blank" style={{ textDecoration: 'underline', color: 'white' }}>
         FTK Discord
       </a>
